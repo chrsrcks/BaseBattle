12d1789514af8bc0136e74c1d2e0edd0173c25bb
chrsrcks <chrsrckes@gmail.com>
2018-12-12 23:06:36 +0100

    fix package name

---
+++ game/Building.java
@@ -0,0 +1,13 @@
+package game;
+
+import gameEngine.Tile;
+
+import java.awt.*;
+
+public class Building extends Tile {
+
+
+    public Building(double _x, double _y, Image _img, int _imgW, int _imgH) {
+        super(_x, _y, _img, _imgW, _imgH);
+    }
+}
---
+++ gameEngine/Tile.java
@@ -0,0 +1,10 @@
+package gameEngine;
+
+import java.awt.*;
+
+public class Tile extends Sprite {
+
+    public Tile(double _x, double _y, Image _img, int _imgW, int _imgH) {
+        super(_x, _y, _img, _imgW, _imgH);
+    }
+}
